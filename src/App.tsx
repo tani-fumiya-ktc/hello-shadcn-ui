3ce49b7aae01729990c75f5855c62974a1ff50d0
tani-fumiya-ktc <fumiya.tani@kinto-technologies.com>
2025-03-04 15:54:29 +0900

    chore: Add Button component with variants and sizes

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,49 @@ function App() {
       <h1>Vite + React</h1>
       <p className="bg-primary text-primary-foreground">hello</p>
       <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
+        <Button size="sm" onClick={() => setCount((count) => count + 1)}>
           count is {count}
+        </Button>
+        <Button size="lg" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
+        <Button size="icon" onClick={() => setCount((count) => count + 1)}>
+          {count}
+        </Button>
+        <Button
+          variant="destructive"
+          size="sm"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count is {count}
+        </Button>
+        <Button
+          className="bg-red-50 font-bold text-[#f0f] hover:bg-[#333]"
+          size="lg"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count is {count}
+        </Button>
+        <Button
+          variant="secondary"
+          size="icon"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          {count}
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          {count}
+        </Button>
+        <Button
+          variant="link"
+          size="icon"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          {count}
         </Button>
       </div>
       <p className="read-the-docs">
